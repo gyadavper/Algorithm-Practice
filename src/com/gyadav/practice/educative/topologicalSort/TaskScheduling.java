884d877a15b7e2c7c56e615a519da668c13ddb7d
gyadav47 <g.yadav47@gmail.com>
2020-07-27 14:40:33 -0400

    finished course

---
+++ src/com/gyadav/practice/educative/topologicalSort/TaskScheduling.java
@@ -0,0 +1,38 @@
+package com.gyadav.practice.educative.topologicalSort;
+
+import java.util.*;
+
+public class TaskScheduling {
+    public static boolean isSchedulingPossible(int tasks, int[][] prerequisites) {
+        if (tasks <= 0)
+            return true;
+        HashMap<Integer, Integer> inDegree = new HashMap<>();
+        HashMap<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < tasks; i++) {
+            inDegree.put(i, 0);
+            graph.put(i, new ArrayList<>());
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            int parent = prerequisites[i][0], child = prerequisites[i][1];
+            graph.get(parent).add(child);
+            inDegree.put(child, inDegree.get(child) + 1);
+        }
+        Queue<Integer> sources = new LinkedList<>();
+        for (Map.Entry<Integer, Integer> entry : inDegree.entrySet()) {
+            if (entry.getValue() == 0)
+                sources.add(entry.getKey());
+        }
+        int c = 0;
+        while (!sources.isEmpty()) {
+            int vertex = sources.poll();
+            c++;
+            List<Integer> children = graph.get(vertex);
+            for (int child : children) {
+                inDegree.put(child, inDegree.get(child) - 1);
+                if (inDegree.get(child) == 0)
+                    sources.add(child);
+            }
+        }
+        return c == tasks;
+    }
+}
